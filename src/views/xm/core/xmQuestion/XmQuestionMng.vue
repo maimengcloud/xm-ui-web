64370894be83e29203870f86888d671f726e3a32
陈裕财 <cyc58469@163.com>
2022-02-16 21:42:28 +0800

    优化

--- src/views/xm/core/xmQuestion/XmQuestionMng.vue
+++ src/views/xm/core/xmQuestion/XmQuestionMng.vue
@@ -277,7 +277,7 @@
 					pageSize:10,//每页数据
 					count:false,//是否需要重新计算总记录数
 					pageNum:1,//当前页码、从1开始计算
-					orderFields:['create_time'],//排序列 如 ['sex','student_id']，必须为数据库字段
+					orderFields:['ltime'],//排序列 如 ['sex','student_id']，必须为数据库字段
 					orderDirs:['desc']//升序 asc,降序desc 如 性别 升序、学生编号降序 ['asc','desc']
 				},
 				load:{ list: false, edit: false, del: false, add: false },//查询中...
