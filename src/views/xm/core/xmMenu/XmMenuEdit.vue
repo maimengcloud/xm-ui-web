6e4a3312ee8c4ea85ab1f390e95b88d21bcc51df
陈裕财 <cyc58469@163.com>
2022-03-19 01:33:39 +0800

    优化

--- src/views/xm/core/xmMenu/XmMenuEdit.vue
+++ src/views/xm/core/xmMenu/XmMenuEdit.vue
@@ -124,6 +124,11 @@
 							<el-form-item label="实际金额" prop="mactAmount" v-if="editForm.calcType==='2'">
 								<el-input-number style="width:200px;"  v-model="editForm.mactAmount" :precision="2" :step="100" :min="0" placeholder="实际金额"></el-input-number>   元 
 							</el-form-item> 
+							<font color="red" style="font-size:10px;">控制规则:
+								<br>下级往上汇总：指需求池的数据来自直接下级的需求池(需求)的数据之和。逐级往上汇总。
+								<br>由任务汇总： 指需求(不包括需求池)的数据来自与该需求关联的任务的数据之和。
+								<br>手工填报：  指需求(不包括需求池)的数据来自手工填报，无论是否关联了任务，都不从任务汇总。
+							</font>
 						</el-collapse-item>
 						<el-collapse-item title="相关链接" name="4"> 
 							<el-form-item label="需求链接" prop="demandUrl"> 
