962fc9135eb8e1e6f63c4c3a07419ab65f0ae278
陈裕财 <cyc58469@163.com>
2022-03-30 15:12:40 +0800

    优化

--- src/views/xm/core/components/XmMenuWorkload.vue
+++ src/views/xm/core/components/XmMenuWorkload.vue
@@ -5,36 +5,26 @@
 			width="500"
 			v-model="formVisible"
 			trigger="manual" >  <el-form :model="editForm"  label-width="100px"  ref="editForm"> 
-					<el-form-item label="汇总方式" prop="calcType"> 
-						<el-radio   v-model="editForm.calcType"  label="3" placeholder="下级往上级汇总" :disabled="editForm.ntype!=='0'">下往上</el-radio>
-						<el-radio   v-model="editForm.calcType"  label="1" placeholder="由任务汇总" :disabled="editForm.ntype!=='0'">任务汇总</el-radio> 
-						<el-radio   v-model="editForm.calcType"  label="2" placeholder="手工填报" :disabled="editForm.ntype!=='0'">手工填报</el-radio>
-					</el-form-item> 
 						
 					<el-form-item label="工时进度" prop="mactRate" >
 						<el-progress style="width:50%;" :stroke-width="15" :percentage="editForm.mactRate?editForm.mactRate:0"></el-progress>
 					</el-form-item>  
 					<el-form-item label="预估工期" prop="budgetHours">
-						<el-input-number :disabled="editForm.calcType!=='2' " style="width:200px;"  v-model="editForm.budgetHours"  :precision="2" :step="8" :min="0" placeholder="预计工期(小时)"></el-input-number>&nbsp;h
+						{{editForm.budgetHours}}&nbsp;小时
 					</el-form-item> 
 					<el-form-item label="预估工时" prop="budgetWorkload">
-						<el-input-number :disabled="editForm.calcType!=='2' " style="width:200px;"  v-model="editForm.budgetWorkload" :precision="2" :step="8" :min="0" placeholder="预计工时(人时)"></el-input-number>  &nbsp;h
+						 {{editForm.budgetWorkload}}&nbsp;小时
 					</el-form-item> 
 					<el-form-item label="实际工时" prop="mactWorkload">
-						<el-input-number :disabled="editForm.calcType!=='2' " style="width:200px;"  v-model="editForm.mactWorkload" :precision="2" :step="8" :min="0" placeholder="实际工时(人时)"></el-input-number>  &nbsp;h
-					</el-form-item> 
-					<font color="blue" style="font-size:10px;">控制规则:
-						<br>下级往上汇总：指工时数据按 &nbsp;用户故事->特性->史诗 &nbsp;这样的汇总关系将数据逐级往上汇总。
-						<br>由任务汇总： 指用户故事的工时数据由任务汇总。
-						<br>手工填报：  指用户故事的工时数据来自手工填报，无论是否关联了任务，都不从任务汇总。
-					</font>
+						 {{editForm.mactWorkload}}&nbsp;小时
+					</el-form-item>  
 					
 					<el-form-item label="" class="padding">
 						<el-button   type="text" @click="cancel">关闭</el-button>
-						<el-button :disabled="editForm.calcType!=='2'" type="primary" @click="submit">确认</el-button>
+						 
 					</el-form-item> 
 				</el-form> 
-				<el-button title="点击填写工时" @click="formVisible=!formVisible"  type="warning" slot="reference" icon="el-icon-edit"><font style="font-size:14px;"></font></el-button> 
+				<el-button title="点击查看工时进度" @click="formVisible=!formVisible"  type="warning" slot="reference" icon="el-icon-search">工时<font style="font-size:14px;"></font></el-button> 
 		</el-popover> 
  </section>
 </template>
