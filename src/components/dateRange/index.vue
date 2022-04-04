8639e34e8d29e3588d3fc21e716b2ecee56d65ee
陈裕财 <cyc58469@163.com>
2022-04-04 19:40:04 +0800

    优化

--- src/components/dateRange/index.vue
+++ src/components/dateRange/index.vue
@@ -4,6 +4,7 @@
       :range-separator="rangeSepaSrator"
       :start-placeholder="startPlaceholder"
       :end-placeholder="endPlaceholder"
+      :default-range="[-30,0]"
       @change="onDateRangeChange"
       :picker-options="pickerOptions"
       ></el-date-picker>
@@ -80,6 +81,14 @@ export default {
     pickerOptions:{
         typeof:Object,
         default:util.pickerOptions('datarange')
+    },
+    autoDefault:{
+        type:Boolean,
+        default:true,
+    },
+    defaultRange:{
+        type:Array,
+        default:[-15,15]
     }
   },
   methods: { 
@@ -92,6 +101,18 @@ export default {
             if(this.value && this.value[this.startKey] && this.value[this.endKey]){
               this.dateRange=[this.value[this.startKey],this.value[this.endKey]]
             }
+          }
+          if( !this.dateRange || this.dateRange.length===0){
+              if(this.autoDefault===true || (this.autoDefault!==false && this.defaultRange && this.defaultRange.length==2)){
+                  var now=new Date();
+                  var start=new Date(); 
+                  var end=new Date(); 
+                  start.setTime(now.getTime() + 3600 * 1000 * 24 * this.defaultRange[0]);
+                  end.setTime(now.getTime() + 3600 * 1000 * 24 * this.defaultRange[1]);
+                  this.dateRange.push(util.formatDate.format(start,this.valueFormat))
+                  this.dateRange.push(util.formatDate.format(end,this.valueFormat))
+                this.onDateRangeChange(this.dateRange);
+              }
           }
           
       },
