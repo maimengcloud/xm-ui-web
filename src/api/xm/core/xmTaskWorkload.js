8b779f1684d5fc38df55a8919b6ecd3d9853af8a
陈裕财 <cyc58469@163.com>
2022-04-23 11:59:27 +0800

    优化

--- src/api/xm/core/xmTaskWorkload.js
+++ src/api/xm/core/xmTaskWorkload.js
@@ -43,3 +43,15 @@ export const batchSetSbillIdNull = params => { return axios.post(`${base}/xm/cor
 
 
 
+
+/**-------------------------前端mng|add|edit界面公共函数---------------请写在下面----------------------------------------------- */
+//初始化页面上的字典
+export const initDicts = (that) => {
+    var itemCodes=[];//在此添加要加载的字典 如['sex','grade','lvl']
+    if(itemCodes.length>0){
+       initSimpleDicts('all',itemCodes).then(res=>{
+           Object.assign(that.dicts,res.data.data)
+       });
+    }
+   };
+
