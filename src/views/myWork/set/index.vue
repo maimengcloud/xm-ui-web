94291d7804351374636b25f77870e94db1be4bf8
陈裕财 <cyc58469@163.com>
2022-05-02 05:56:41 +0800

    优化

--- src/views/myWork/set/index.vue
+++ src/views/myWork/set/index.vue
@@ -342,7 +342,7 @@ import md5 from "js-md5";
 				 this.$prompt('请输入密码', '提示', {
 					confirmButtonText: '确定',
 					cancelButtonText: '取消',  
-					}).then(({ value }) => {
+					}).then(({ value }) => { 
 						 let params={ 
 							password:md5(value),  
 							userloginid:user.userid,
