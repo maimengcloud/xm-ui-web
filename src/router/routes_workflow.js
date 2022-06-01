623b1680fc751f381cffa73f6e0e98f1b92c608f
陈裕财 <cyc58469@163.com>
2022-06-02 07:41:51 +0800

    审批优化

--- src/router/routes_workflow.js
+++ src/router/routes_workflow.js
@@ -14,12 +14,7 @@ export default {
 			iconCls: 'fa el-icon-menu',
 			//leaf: true,//只有一个节点
 			children: [
-				{ path: 'task/TaskListAssigneeToMe', component: _import("mdp/workflow/ru/task/TaskListAssigneeToMe"), name: 'TaskListAssigneeToMe', meta: { title: 'TaskListAssigneeToMe',openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}' } },
-				{ path: 'task/TaskListAssigneeToMeCalendar', component: _import("mdp/workflow/ru/task/TaskListAssigneeToMeCalendar"), name: 'TaskListAssigneeToMeCalendar', meta: { title: 'TaskListAssigneeToMeCalendar' ,openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}'} },
-				{ path: 'task/TaskListAssigneeToMeClaim', component: _import("mdp/workflow/ru/task/TaskListAssigneeToMeClaim"), name: 'TaskListAssigneeToMeClaim', meta: { title: 'TaskListAssigneeToMeClaim' ,openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}'} ,hidden:true},
-				{ path: 'procinstParames/ProcinstParamesExecutionSetRoute', component: _import("mdp/workflow/ru/procinstParames/ProcinstParamesExecutionSetRoute"), name: 'ProcinstParamesExecutionSetRoute', hidden: true, meta: { title: 'ProcinstParamesExecutionSetRoute', openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}',noCache: true } },
-				{ path: 'procinstParames/ProcinstParamesStartSetRoute', component: _import("mdp/workflow/ru/procinstParames/ProcinstParamesStartSetRoute"), name: 'ProcinstParamesStartSetRoute', hidden: true, meta: { title: 'ProcinstParamesStartSetRoute', openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}',noCache: true } },
-				{ path: 'task/TaskMng', component: _import("mdp/workflow/ru/task/TaskMng"), name: 'TaskMng', meta: { title: 'TaskMng', openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}', roles: ['flowAdmin', 'branchAdmin', 'platformAdmin'] } }
+				{ path: 'task/TaskListAssigneeToMe', component: _import("mdp/workflow/ru/task/TaskListAssigneeToMe"), name: 'TaskListAssigneeToMe', meta: { title: '审批中心',icon: 'task',openTab:true,outUrl:'${curlDomain}/workflow/'+process.env.VERSION+'/#/${router.path}' } },
 			]
 		}, 
 	]
