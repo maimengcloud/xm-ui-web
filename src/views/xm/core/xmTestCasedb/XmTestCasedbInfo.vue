07d2c3df5c1b23a9e1d4432245a5c2ba0f53ef01
陈裕财 <cyc58469@163.com>
2022-07-31 16:40:20 +0800

    优化

--- src/views/xm/core/xmTestCasedb/XmTestCasedbInfo.vue
+++ src/views/xm/core/xmTestCasedb/XmTestCasedbInfo.vue
@@ -19,17 +19,19 @@
                 <el-menu-item index="caseFlow">用例评审</el-menu-item>
                 <el-menu-item index="testPlan">测试计划</el-menu-item>
                 <el-menu-item index="testRpt">统计报表</el-menu-item> 
-                
-                <el-menu-item index="首页" @click.native="goHome">
-                <span slot="title"
-                    ><i class="el-icon-s-home"></i></span
-                >
-                </el-menu-item> 
-                <el-menu-item index="上一页" class="hidden-md-and-down"  @click.native="goBack">
-                <span slot="title"
-                    ><i class="el-icon-back"></i></span
-                >
-                </el-menu-item> 
+                <el-submenu index="回退">
+                <template slot="title"><i class="el-icon-back"></i>回退</template>
+                    <el-menu-item index="上一页"  @click.native="goBack">
+                        <span slot="title"
+                            ><i class="el-icon-back"></i>上一页</span
+                        >
+                    </el-menu-item> 
+                    <el-menu-item index="首页" @click.native="goHome">
+                        <span slot="title"
+                            ><i class="el-icon-s-home"></i>首页</span
+                        >
+                    </el-menu-item> 
+                </el-submenu>
             </el-menu>
         </el-row>
 		<el-row ref="xmTestCasedbTable">
