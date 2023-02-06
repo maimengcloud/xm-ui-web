ffedb1b9e31a844c2eaa3ee7a70ed5a5b58791b2
陈裕财 <cyc58469@163.com>
2023-02-06 15:18:06 +0800

    优化

--- src/api/cpd.js
+++ src/api/cpd.js
@@ -15,45 +15,45 @@ export const getNoticeMsg = params => {
       var curlDomain=window.location.protocol+"//"+window.location.host; //   返回https://mp.csdn.net 
       if(item.objType=='1'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmProject/XmProjectInfoRoute",query:{id:item.bizId}})
+            that.$router.push({path:"/xm/core/project/overview",query:{projectId:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmProject/XmProjectInfoRoute?id="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/project/overview?projectId="+item.bizId)
             }
       }else if(item.objType=='2'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmTask/XmTaskDetailRoute",query:{id:item.bizId}})
+            that.$router.push({path:"/xm/core/task/detail",query:{id:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmTask/XmTaskDetailRoute?id="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/task/detail?id="+item.bizId)
             }
       }else if(item.objType=='3'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmProduct/XmProductInfoRoute",query:{id:item.bizId}})
+            that.$router.push({path:"/xm/core/product/overview",query:{productId:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmProduct/XmProductInfoRoute?id="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/product/overview?productId="+item.bizId)
             }
       }else if(item.objType=='4'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmMenu/XmMenuDetailRoute",query:{menuId:item.bizId}})
+            that.$router.push({path:"/xm/core/menu/detail",query:{menuId:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmMenu/XmMenuDetailRoute?menuId="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/menu/detail?menuId="+item.bizId)
             }
       }else if(item.objType=='5'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmQuestion/XmQuestionDetailRoute",query:{id:item.bizId}})
+            that.$router.push({path:"/xm/core/question/detail",query:{id:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmQuestion/XmQuestionDetailRoute?id="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/question/detail?id="+item.bizId)
             }
       }else if(item.objType=='6'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmIteration/XmIterationInfoRoute",query:{id:item.bizId}})
+            that.$router.push({path:"/xm/core/iteration/detail",query:{id:item.bizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmIteration/XmIterationInfoRoute?id="+item.bizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/iteration/detail?id="+item.bizId)
             }
       }else if(item.objType=='7'){
             if(process.env.CONTEXT=='xm'){
-            that.$router.push({path:"/xm/core/xmGroup/xmGroupRoute",query:{projectId:item.pbizId}})
+            that.$router.push({path:"/xm/core/group/detail",query:{projectId:item.pbizId}})
             }else{
-            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/xmGroup/xmGroupRoute?projectId="+item.pbizId)
+            window.open(curlDomain+"/xm/"+process.env.VERSION+"/#/xm/core/group/detail?projectId="+item.pbizId)
             }
       }
 } 
