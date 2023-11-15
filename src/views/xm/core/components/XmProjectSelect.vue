f3c1ba5da36a7dce35e1c7ab4bf7c256d55b1e1f
陈裕财 <cyc58469@163.com>
2023-11-15 11:52:28 +0800

    优化

--- src/views/xm/core/components/XmProjectSelect.vue
+++ src/views/xm/core/components/XmProjectSelect.vue
@@ -1,13 +1,13 @@
 <template> 
 <span>
-   <mdp-select-table v-model="myVal" ref="selectTableRef" show-style="tag" placeholder="选择项目" :props="{id:'id',name:'productName'}" :load-fun="listXmProject" :params="{linkProjectId:linkProjectId}" @change2="onChange2" @clear="onClear" @change="onChange" @click="onRowClick">
+   <mdp-select-table v-model="myVal" ref="selectTableRef" show-style="tag" placeholder="选择项目" :props="{id:'id',name:'projectName'}" :load-fun="listXmProject" :params="{linkProductId:linkProductId}" @change2="onChange2" @clear="onClear" @change="onChange" @click="onRowClick">
       <el-row slot="toolbar"><el-button size="mini" icon="el-icon-plus" @click="$refs['addDialog'].open()">创建项目</el-button><el-button size="mini" icon="el-icon-search" @click="$refs['selectTableRef'].$refs['tableDialog'].open()">更多项目</el-button></el-row>
    </mdp-select-table> 
    
    <mdp-dialog ref="addDialog">
     <template v-slot="visible,data,dialog">
       <xm-project-add op-type="add"
-        :xm-product="{ id: linkProductId, name: '' }"
+        :xm-project="{ id: linkProductId, name: '' }"
         @cancel="dialog.close()"
         @submit="afterAddSubmit"
       >
